2ef5c563e976ea6d5876441123d6381aa31610f8
LaRenzo <dlarenzo@outlook.com>
2024-03-09 21:12:17 -0700

    finished navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter, Raleway } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 const raleway = Raleway({ subsets: ["latin"], display: "swap" });
 
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={raleway.className}>{children}</body>
+      <body className={raleway.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }

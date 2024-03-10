1d1286fec01f1ad974660329dff164bebe458be7
LaRenzo <dlarenzo@outlook.com>
2024-03-09 19:42:14 -0700

    completed hero section

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <Hero
+        heading="Lighthouse Photography"
+        message="Capturing moments today tha will wow your heart tomorrow."
+      />
     </main>
   );
 }
